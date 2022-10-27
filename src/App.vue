b304a9ea6cd3fc80279f4eff09337319270da90b
xdavidwu <xdavidwuph@gmail.com>
2022-10-27 23:50:45 +0800

    vuetify the app layout

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,54 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+import {
+  VApp,
+  VAppBar,
+  VAppBarNavIcon,
+  VContainer,
+  VList,
+  VListItem,
+  VMain,
+  VNavigationDrawer,
+  VToolbarTitle,
+} from 'vuetify/components';
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-        <RouterLink :to="{ name: 'about' }">About</RouterLink>
-        <RouterLink :to="{ name: 'explore' }">Resource Explorer</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <VApp>
+    <VAppBar>
+      <VAppBarNavIcon @click="drawer = !drawer" />
+      <VToolbarTitle>{{ title }}</VToolbarTitle>
+    </VAppBar>
+    <VNavigationDrawer v-model="drawer">
+      <VList>
+        <VListItem :to="{ name: 'home' }">Home</VListItem>
+        <VListItem :to="{ name: 'about' }">About</VListItem>
+        <VListItem :to="{ name: 'explore' }">Resource Explorer</VListItem>
+      </VList>
+    </VNavigationDrawer>
+    <VMain>
+      <VContainer fluid>
+        <RouterView />
+      </VContainer>
+    </VMain>
+  </VApp>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script lang="ts">
+export default {
+  props: {
+    title: {
+      type: String,
+      default: import.meta.env.VITE_APP_BRANDING ?? 'Kubernetes SPA Client',
+    },
+  },
+  data: () => ({ drawer: null }),
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+<style>
+/* XXX: is absent of this bug in VListItem? */
+:root {
+  --v-theme-overlay-multiplier: 1;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import '@mdi/font/css/materialdesignicons.css';
 
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { createVuetify } from 'vuetify';
+
+import App from './App.vue';
+import colors from 'vuetify/lib/util/colors';
+import router from './router';
 
 import './assets/main.css'
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(createVuetify({
+  theme: {
+    defaultTheme: 'dark',
+    themes: {
+      dark: {
+        overlayMultiplier: 1,
+      },
+    },
+  },
+}));
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
