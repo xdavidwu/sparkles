e5dd1a601c2825fd863a98998b88dace29c06307
xdavidwu <xdavidwuph@gmail.com>
2023-01-27 14:44:29 +0800

    move helm-wasm .d.ts to its repo

--- src/views/HelmList.d.ts
+++ src/views/HelmList.d.ts
@@ -1,9 +1,1 @@
-declare function listReleasesForNamespace(namespace: string): Promise<string>;
-
-interface ConnectionConfig {
-  basePath: string,
-  accessToken?: string | null,
-  impersonation: { asUser?: string, asGroup?: string },
-}
-
-declare function configConnection(config: ConnectionConfig): void;
+../../helm-wasm/index.d.ts
