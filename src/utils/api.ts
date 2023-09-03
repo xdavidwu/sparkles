793ef926bb823986564f7ec597fe4b2c6bebfbe7
Pinghao Wu <xdavidwuph@gmail.com>
2023-09-03 20:55:31 +0800

    errorIsUnsupportedApi: further check message
    on a generic 404 with v1status, including unsupported kind, apiserver
    returns this message
    when a resource named not found, but the kind is supported, it returns a
    much more specific message including the name and kind.
    
    this message seems to be there since 0.15.0, should be okay-ish to rely
    on

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,8 @@ import { V1StatusFromJSON, ResponseError } from '@/kubernetes-api/src';
 export const errorIsApiUnsupported = async (err: any) => {
   if (err instanceof ResponseError && err.response.status === 404) {
     const status = V1StatusFromJSON(JSON.parse(await err.response.text()));
-    if (status.status === 'Failure' && status.reason === 'NotFound') {
+    if (status.status === 'Failure' && status.reason === 'NotFound' &&
+        status.message === 'the server could not find the requested resource') {
       return true;
     }
   }
