cebb49d98221d953b0775a7477cb6a52a0ef6ae1
Pinghao Wu <xdavidwuph@gmail.com>
2023-11-05 14:36:01 +0800

    LinkedImage: handle host:port, link tags, fallback to oci dist

--- src/components/LinkedImage.vue
+++ src/components/LinkedImage.vue
@@ -6,30 +6,62 @@ const props = defineProps<{
 }>();
 
 const url = computed(() => {
-  const components = props.image.split(':')[0].split('/');
+  // https://github.com/opencontainers/distribution-spec/blob/main/spec.md
+  const nameTagRegExp = /^(?<image>[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*(\/[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*)*)(:(?<tag>[a-zA-Z0-9_][a-zA-Z0-9._-]{0,127}))?$/;
+  let match = props.image.match(nameTagRegExp);
 
-  // normalization
-  if (components.length >= 2 && components[0].indexOf('.') === -1) {
-    components.unshift('docker.io');
-  } else if (components.length === 1) {
-    components.unshift('docker.io', 'library');
+  let host;
+  let image;
+  if (!match) {
+    // possibly with host:port on first part?
+    const firstslash = props.image.indexOf('/');
+    if (firstslash === -1) {
+      return null;
+    }
+
+    host = props.image.slice(0, firstslash);
+    match = props.image.slice(firstslash + 1).match(nameTagRegExp);
+    if (!match) {
+      return null;
+    }
+    image = match.groups!.image;
+  } else {
+    const firstslash = match.groups!.image.indexOf('/');
+
+    if (firstslash === -1) {
+      host = 'docker.io';
+      image = `library/${match.groups!.image}`;
+    } else {
+      host = match.groups!.image.slice(0, firstslash);
+      // TODO better name validation?
+      if (host.indexOf('.') === -1) {
+        host = 'docker.io';
+        image = match.groups!.image;
+      } else {
+        image = match.groups!.image.slice(firstslash + 1);
+      }
+    }
   }
+  const tagTail = match.groups!.tag ? `:${match.groups!.tag}` : '';
 
-  switch (components[0]) {
+  switch (host) {
     case 'docker.io':
-      return `https://hub.docker.com/r/${components.slice(1).join('/')}`;
+      return `https://hub.docker.com/r/${image}`;
     case 'public.ecr.aws':
-      return `https://gallery.ecr.aws/${components.slice(1).join('/')}`;
+      return `https://gallery.ecr.aws/${image}`;
     case 'registry.k8s.io':
     case 'k8s.gcr.io':
-      return `https://us-central1-docker.pkg.dev/k8s-artifacts-prod/images/${components.slice(1).join('/')}`;
+      return `https://us-central1-docker.pkg.dev/k8s-artifacts-prod/images/${image}${tagTail}`;
     case 'quay.io':
     case 'gcr.io':
     case 'ghcr.io':
-    case components[0].match(/.-docker\.pkg\.dev$/)?.input:
-      return `https://${components.join('/')}`;
+    case host.match(/.-docker\.pkg\.dev$/)?.input:
+      return `https://${host}/${image}${tagTail}`;
     default:
-      return null;
+      // we don't actually know if it is https or http /shrug
+      return match.groups!.tag ?
+        `https://${host}/v2/${image}/manifests/${match.groups!.tag}` :
+        `https://${host}/v2/${image}/tags/list`;
   }
 });
 </script>
