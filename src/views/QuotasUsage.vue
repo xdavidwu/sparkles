38a8d172833e277dd49c3537151de87582f521b9
Pinghao Wu <xdavidwuph@gmail.com>
2023-12-02 23:55:00 +0800

    QuotasUsage: better layout for multiple quotas

--- src/views/QuotasUsage.vue
+++ src/views/QuotasUsage.vue
@@ -95,16 +95,18 @@ watch(selectedNamespace, async (namespace) => {
 
 <template>
   <!-- TODO: mark scoped quotas -->
-  <VCard v-for="quota in quotas" :key="uniqueKeyForObject(quota)"
-    :title="quota.metadata!.name" class="mb-4">
-    <VCardText class="d-flex justify-center justify-space-evenly align-center flex-wrap">
-      <QuotaDoughnut v-for="(value, key) of quota.spec!.hard" :key="key"
-        class="my-2" :title="key as string" :details="podsResourceUsage[key]"
-        :used="real(quota.status!.used![key])!" :total="real(value)!">
-        {{ quota.status!.used![key] }}/{{ value }}
-      </QuotaDoughnut>
-    </VCardText>
-  </VCard>
+  <div class="d-flex align-center flex-wrap ga-4">
+    <VCard v-for="quota in quotas" :key="uniqueKeyForObject(quota)"
+      :title="quota.metadata!.name">
+      <VCardText class="d-flex justify-space-evenly align-center flex-wrap ga-8 px-8">
+        <QuotaDoughnut v-for="(value, key) of quota.spec!.hard" :key="key"
+          :title="key as string" :details="podsResourceUsage[key]"
+          :used="real(quota.status!.used![key])!" :total="real(value)!">
+          {{ quota.status!.used![key] }}/{{ value }}
+        </QuotaDoughnut>
+      </VCardText>
+    </VCard>
+  </div>
   <template v-if="!quotas.length">
     No quota enforcement found in this namespace.
   </template>
