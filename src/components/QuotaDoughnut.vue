0fe9d2ad9df7cb1de4b0d767efae3b167753f66c
Pinghao Wu <xdavidwuph@gmail.com>
2023-12-03 02:24:26 +0800

    QuotaDoughnut: html tooltip

--- src/components/QuotaDoughnut.vue
+++ src/components/QuotaDoughnut.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
+import { VOverlay } from 'vuetify/components';
 import { Doughnut } from 'vue-chartjs';
+import type { TooltipModel } from 'chart.js';
 // @ts-expect-error Missing type definitions
 import colors from 'vuetify/lib/util/colors';
 
@@ -36,21 +38,58 @@ const chartData = computed(() => ({
   }],
 }));
 
-// TODO: external tooltip to make it not restricted to canvas?
+const tooltip = ref<Pick<TooltipModel<'doughnut'>, 'title' | 'body' | 'opacity' | 'caretX' | 'caretY' | 'labelColors'> | null>(null);
+const tooltipHandler = (context: { tooltip: TooltipModel<'doughnut'> }) => {
+  const pending = {
+    title: context.tooltip.title,
+    body: context.tooltip.body,
+    opacity: context.tooltip.opacity,
+    caretX: context.tooltip.caretX,
+    caretY: context.tooltip.caretY,
+    labelColors: context.tooltip.labelColors,
+  };
+  // prevent unneeded reactive ping, to avoid dom op + chartjs update loop
+  if (JSON.stringify(pending) !== JSON.stringify(tooltip.value)) {
+    tooltip.value = pending;
+  }
+}
 </script>
 
 <template>
   <div>
-    <h4 class="doughnut-title text-center mb-1">{{ title }}</h4>
+    <h4 class="doughnut-title text-center mb-1">
+      {{ title }}
+      <VOverlay location-strategy="connected" location="bottom left"
+        activator="parent" content-class="pointer-events-none"
+        :offset="tooltip ? [tooltip.caretY, -tooltip.caretX] : [0, 0]"
+        :scrim= "false" :modelValue="tooltip != null && tooltip.opacity != 0">
+        <div class="text-white bg-grey-darken-3 px-2 py-1 text-caption">
+          <div v-for="(title, i) in tooltip?.title" :key="i">
+            <div>{{ title }}</div>
+            <div class="d-flex align-center">
+              <div class="legend d-inline-block mr-1"
+                :style="{
+                  'background-color': tooltip?.labelColors[i].backgroundColor as string,
+                  'border-color': tooltip?.labelColors[i].borderColor as string,
+                }"></div>
+              {{ tooltip?.body[i].lines.join('') }}
+            </div>
+          </div>
+        </div>
+      </VOverlay>
+    </h4>
     <div class="doughnut">
       <div class="doughnut-graph">
         <Doughnut :data="chartData" :options="{
           cutout: '66%',
-          plugins: { legend: { display: false } },
+          plugins: {
+            legend: { display: false },
+            tooltip: { enabled: false, external: tooltipHandler },
+          },
         }"/>
       </div>
       <div class="doughnut-text text-center">
-        <h2 :style="`color: ${color}`">
+        <h2 :style="{ color }">
           {{ percentage }}%
         </h2>
         <slot />
@@ -72,7 +111,6 @@ const chartData = computed(() => ({
 
 .doughnut-graph {
   position: relative;
-  z-index: 1;
 }
 
 .doughnut-text {
@@ -80,5 +118,18 @@ const chartData = computed(() => ({
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+
+.legend {
+  width: 12px;
+  height: 12px;
+  border-width: 1px;
+  border-style: solid;
+}
+</style>
+
+<style>
+.pointer-events-none {
+  pointer-events: none !important;
 }
 </style>
