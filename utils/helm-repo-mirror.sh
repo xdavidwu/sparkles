0e98ae8d336e1cd5cae65a4b01eb5b1292f1313d
Pinghao Wu <xdavidwuph@gmail.com>
2024-01-20 15:48:09 +0800

    add utility to incrementally mirror helm chart repo

---
+++ utils/helm-repo-mirror.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+set -e
+
+help() {
+	printf 'Usage: %s REPO PATH URL\n' "$0"
+	printf '\n'
+	printf 'Update latest charts from REPO to PATH for being served at URL\n'
+}
+
+if [ $# -ne 3 ]; then
+	help >&2
+	exit 1
+fi
+
+REPO="$1"
+OUTPUT="$2"
+URL="$3"
+
+mkdir -p -- "$OUTPUT"
+
+INDEX=$(wget -q -O - -- "$REPO/index.yaml")
+TEMPDIR=$(mktemp -p "$OUTPUT" -d)
+
+IFS='
+'
+
+for i in $(echo "$INDEX" | yq '.entries.*[0].urls[0]' -r); do
+	FILENAME=$(basename "$i")
+	if [ -f "$OUTPUT/index.yaml" ] && [ -f "$OUTPUT/$FILENAME" ]; then
+		printf '%s already exists, skipping\n' "$FILENAME"
+		continue
+	fi
+	printf 'Downloading %s\n' "$FILENAME"
+	wget -q -O "$TEMPDIR/$FILENAME" -- "$i"
+done
+
+if [ -f "$PATH/index.yaml" ]; then
+	helm repo index --url "$URL" --merge "$OUTPUT/index.yaml" -- "$TEMPDIR"
+else
+	helm repo index --url "$URL" -- "$TEMPDIR"
+fi
+
+mv "$TEMPDIR"/* "$OUTPUT"
+rmdir "$TEMPDIR"
