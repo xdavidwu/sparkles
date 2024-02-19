fde13caee1ef0be57d85630353c8a5dcef4291c1
Pinghao Wu <xdavidwuph@gmail.com>
2024-02-19 21:33:05 +0800

    OIDCApp: do oidc stuff on oidc only

--- src/OIDCApp.vue
+++ src/OIDCApp.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
 import App from '@/App.vue';
 import { ref, onMounted } from 'vue';
-import { useApiConfig } from '@/stores/apiConfig';
+import { useApiConfig, AuthScheme } from '@/stores/apiConfig';
 
 const oidcCompleted = ref(false);
+const apiConfigStore = useApiConfig();
 
 onMounted(async () => {
-  const base = (new URL(import.meta.env.BASE_URL, window.location.origin)).pathname.replace(/\/$/, '');
-  const path = window.location.pathname.replace(base, '');
-  if (path === '/oidc/callback') {
-    const user = await useApiConfig().userManager.signinRedirectCallback();
-    window.location.replace(user.state as string);
-  } else {
-    await useApiConfig().getBearerToken();
+  if (apiConfigStore.authScheme === AuthScheme.OIDC) {
+    const base = (new URL(import.meta.env.BASE_URL, window.location.origin)).pathname.replace(/\/$/, '');
+    const path = window.location.pathname.replace(base, '');
+    if (path === '/oidc/callback') {
+      const user = await apiConfigStore.userManager.signinRedirectCallback();
+      window.location.replace(user.state as string);
+    } else {
+      await apiConfigStore.getIdToken();
+    }
   }
   oidcCompleted.value = true;
 });
