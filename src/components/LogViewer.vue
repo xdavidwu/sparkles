c95d5030383a3eab20627ef7115ce4a9d9b83e1f
Pinghao Wu <xdavidwuph@gmail.com>
2024-03-04 22:53:25 +0800

    LogViewer: expect abort on response in addition to body
    this may happen if the container yield no log at all

--- src/components/LogViewer.vue
+++ src/components/LogViewer.vue
@@ -3,7 +3,7 @@ import XTerm from '@/components/XTerm.vue';
 import type { Terminal } from '@xterm/xterm';
 import { useAbortController } from '@/composables/abortController';
 import { useApiConfig } from '@/stores/apiConfig';
-import { CoreV1Api } from '@/kubernetes-api/src';
+import { CoreV1Api, FetchError } from '@/kubernetes-api/src';
 
 const props = defineProps<{
   containerSpec: {
@@ -21,13 +21,25 @@ const { signal } = useAbortController();
 
 const display = async (terminal: Terminal) => {
   terminal.textarea!.disabled = true;
+
   const config = await useApiConfig().getConfig();
-  const response = (await new CoreV1Api(config).readNamespacedPodLogRaw({
-    namespace: props.containerSpec.namespace,
-    name: props.containerSpec.pod,
-    container: props.containerSpec.container,
-    follow: true,
-  }, { signal: signal.value })).raw.body!.pipeThrough(new TextDecoderStream()).getReader();
+  let response;
+  try {
+    response = (await new CoreV1Api(config).readNamespacedPodLogRaw({
+      namespace: props.containerSpec.namespace,
+      name: props.containerSpec.pod,
+      container: props.containerSpec.container,
+      follow: true,
+    }, {
+      signal: signal.value,
+    })).raw.body!.pipeThrough(new TextDecoderStream()).getReader();
+  } catch (e) {
+    if (e instanceof FetchError &&
+        e.cause instanceof DOMException && e.cause.name === 'AbortError') {
+      return;
+    }
+    throw e;
+  }
 
   // eslint-disable-next-line no-constant-condition
   while (true) {
