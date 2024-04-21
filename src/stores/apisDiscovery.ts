19fa0715335976bbc7b3d588b9be8fe93602585c
Pinghao Wu <xdavidwuph@gmail.com>
2024-04-21 16:47:24 +0800

    apisDiscovery: avoid race

--- src/stores/apisDiscovery.ts
+++ src/stores/apisDiscovery.ts
@@ -1,36 +1,39 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
 import { useApiConfig } from '@/stores/apiConfig';
 import { CoreApi, ApisApi, VersionApi, type VersionInfo } from '@/kubernetes-api/src';
 import { toV2Discovery, type V2APIGroupDiscoveryList, type V2APIGroupDiscovery } from '@/utils/discoveryV2';
 
 export const useApisDiscovery = defineStore('apisDiscovery', () => {
-  const groups = ref<Array<V2APIGroupDiscovery>>([]);
-  const versionInfo = ref<VersionInfo | null>(null);
+  let groups: Promise<Array<V2APIGroupDiscovery>> | null = null;
+  let versionInfo: Promise<VersionInfo> | null = null;
 
-  const getGroups = async () => {
-    if (groups.value.length !== 0) {
-      return groups.value;
+  const getGroups = () => {
+    if (!groups) {
+      groups = (async () => {
+        const config = await useApiConfig().getConfig();
+        const listing: Array<V2APIGroupDiscovery> = [];
+        (await Promise.all([new CoreApi(config), new ApisApi(config)].map(async (api) => {
+          const response = (await api.withPreMiddleware(toV2Discovery).getAPIVersionsRaw({})).raw;
+          const list = (await response.json()) as V2APIGroupDiscoveryList;
+          if (list.kind !== 'APIGroupDiscoveryList') {
+            throw new Error(`Unexpected kind on discovery: ${list.kind}, make sure server supports AggregatedDiscoveryEndpoint`);
+          }
+          return list.items;
+        }))).forEach((items) => listing.push(...items));
+        return listing;
+      })();
     }
-    const config = await useApiConfig().getConfig();
-    (await Promise.all([new CoreApi(config), new ApisApi(config)].map(async (api) => {
-      const response = (await api.withPreMiddleware(toV2Discovery).getAPIVersionsRaw({})).raw;
-      const list = (await response.json()) as V2APIGroupDiscoveryList;
-      if (list.kind !== 'APIGroupDiscoveryList') {
-        throw new Error(`Unexpected kind on discovery: ${list.kind}, make sure server supports AggregatedDiscoveryEndpoint`);
-      }
-      return list.items;
-    }))).forEach((items) => groups.value.push(...items));
-    return groups.value;
+    return groups;
   };
 
   const getVersionInfo = async () => {
-    if (versionInfo.value !== null) {
-      return versionInfo.value;
+    if (!versionInfo) {
+      versionInfo = (async () => {
+        const config = await useApiConfig().getConfig();
+        return await new VersionApi(config).getCode();
+      })();
     }
-    const config = await useApiConfig().getConfig();
-    versionInfo.value = await new VersionApi(config).getCode();
-    return versionInfo.value;
+    return versionInfo;
   };
 
   return { getGroups, getVersionInfo };
