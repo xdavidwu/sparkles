a0f8b75daf60ff9b1220a535378cc98f19433041
Pinghao Wu <xdavidwuph@gmail.com>
2024-04-21 14:12:28 +0800

    text: fix truncate
    another drunk moment?

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,5 +1,5 @@
 export const truncate = (s: string, toLength: number) =>
-  s.length > toLength ? `...${s.substring(s.length - (toLength - 3))}` : s;
+  s.length > toLength ? `${s.substring(0, toLength - 3)}...` : s;
 
 export const truncateStart = (s: string, toLength: number) =>
   s.length > toLength ? `...${s.substring(s.length - (toLength - 3))}` : s;
