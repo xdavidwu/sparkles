b73b90fcf6f0ab8bcc420a66a6ac49e8ab0b7bec
Pinghao Wu <xdavidwuph@gmail.com>
2024-05-26 14:30:30 +0800

    SpeedDialFab: make props affect fab by default

--- src/components/SpeedDialFab.vue
+++ src/components/SpeedDialFab.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { VSpeedDial, VFab } from 'vuetify/components';
 import FixedFab from '@/components/FixedFab.vue';
+
+defineOptions({ inheritAttrs: false });
 </script>
 
 <template>
   <VSpeedDial scrim location="top end" :offset="[ 24, 44 ]">
     <template #activator="{ props }">
-      <FixedFab icon="mdi-dots-vertical" v-bind="props" />
+      <FixedFab icon="mdi-dots-vertical" v-bind="{ ...props, ...$attrs }" />
     </template>
     <slot />
     <VFab key="0" icon="$close" color="primary" absolute class="fab-overlay" />
