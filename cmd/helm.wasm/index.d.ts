949b84bc478a079ac6889cd3d72d121622bbe9f2
Pinghao Wu <xdavidwuph@gmail.com>
2024-06-09 20:08:38 +0800

    helm.wasm: remove unused type def
    now defined in src/utils/helmWasm

--- cmd/helm.wasm/index.d.ts
+++
@@ -1,8 +0,0 @@
-interface ConnectionConfig {
-  basePath: string,
-  accessToken?: string | null,
-  impersonation: { asUser?: string, asGroup?: string },
-}
-
-declare function _helm_configConnection(config: ConnectionConfig): void;
-declare function _helm_renderTemplate(charts: Array<string>, values: string, options: string, capabilities: string): Promise<string>;
