192192b0d8ceac6c88d7d3682abb21f04f19a528
Pinghao Wu <xdavidwuph@gmail.com>
2024-06-19 00:30:04 +0800

    stores: helmRepository: show a friendly error on index failure

--- src/stores/helmRepository.ts
+++ src/stores/helmRepository.ts
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia';
 import { shallowRef } from 'vue';
 import { useApisDiscovery } from '@/stores/apisDiscovery';
 import { useErrorPresentation } from '@/stores/errorPresentation';
+import { PresentedError } from '@/utils/PresentedError';
 import { type ChartVersion, type IndexFile } from '@/utils/helm';
 import { parse } from 'yaml';
 import { satisfies } from 'semver';
@@ -18,7 +19,15 @@ export const useHelmRepository = defineStore('helmRepository', () => {
     try {
       index = JSON.parse(text);
     } catch {
-      index = parse(text);
+      try {
+        index = parse(text);
+      } catch (e) {
+        // a guess for SPA fallback html
+        if (response.headers.get('Content-Type') == 'text/html') {
+          throw new PresentedError(`Failed to obtain Helm repo index at ${indexURL}, maybe it isn't set up by instance admin yet?`);
+        }
+        throw e;
+      }
     }
 
     const versionInfo = await useApisDiscovery().getVersionInfo();
