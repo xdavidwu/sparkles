6303fd70c6d44015f7ce646b839538f0bebb2d90
Pinghao Wu <xdavidwuph@gmail.com>
2024-06-19 15:27:55 +0800

    HelmCreate: detect step completion

--- src/views/HelmCreate.vue
+++ src/views/HelmCreate.vue
@@ -1,15 +1,37 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { computed, ref, watch } from 'vue';
 import { VStepper } from 'vuetify/components';
 import HelmRepository from '@/components/HelmRepository.vue';
 import { useAppTabs } from '@/composables/appTabs';
 import { type ChartVersion, parseChartTarball } from '@/utils/helm';
 import { renderTemplate } from '@/utils/helmWasm';
 
+enum Step {
+  SELECT_CHART = 1,
+  SET_VALUES,
+  SET_NAME,
+}
+
 // TODO don't
 const { appBarHeightPX } = useAppTabs();
 
 const selectedChart = ref<ChartVersion | undefined>();
+const step = ref<Step>(Step.SELECT_CHART);
+
+const stepCompleted = computed(() => {
+  switch (step.value) {
+  case Step.SELECT_CHART:
+    return !!selectedChart.value;
+  default:
+    return true;
+  }
+});
+
+const steps = [
+  { title: 'Select a chart', value: Step.SELECT_CHART },
+  { title: 'Set values', value: Step.SET_VALUES },
+  { title: 'Name your release', value: Step.SET_NAME },
+];
 
 const load = async (c: ChartVersion) => {
   const response = await fetch(c.urls[0]);
@@ -28,8 +50,8 @@ watch(selectedChart, () => selectedChart.value && load(selectedChart.value));
 </script>
 
 <template>
-  <VStepper :items="['Select a chart', 'Set values', 'Name your release']">
-    <template #[`item.1`]>
+  <VStepper :items="steps" v-model="step" :disabled="!stepCompleted">
+    <template #[`item.${Step.SELECT_CHART}`]>
       <HelmRepository v-model="selectedChart"
         :style="`height: calc(100dvh - ${appBarHeightPX}px - 32px - 132px)`" />
     </template>
