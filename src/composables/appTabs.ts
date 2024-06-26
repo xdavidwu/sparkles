5ec0068ebd4957cc5d1ef017a2e0ecfc5124091f
Pinghao Wu <xdavidwuph@gmail.com>
2024-06-26 20:22:57 +0800

    appTabs: make appBarHeightPX reflect actual usage

--- src/composables/appTabs.ts
+++ src/composables/appTabs.ts
@@ -3,24 +3,20 @@ import { computed, ref, onUnmounted } from 'vue';
 import { useDisplay } from 'vuetify';
 
 const store = defineStore('app-tabs', () => {
+  const { xs: expandAppBar } = useDisplay();
   const appTabsUsed = ref(false);
-  return { appTabsUsed };
+  const appBarHeightPX = computed(() =>
+    expandAppBar.value && appTabsUsed.value ? 64 + 48 : 64);
+  return { appTabsUsed, expandAppBar, appBarHeightPX };
 });
 
-const commonSetup = () => {
-  const { xs: expandAppBar } = useDisplay();
-  const appBarHeightPX = computed(() => expandAppBar.value ? 64 + 48 : 64);
-
-  return { expandAppBar, appBarHeightPX };
-};
-
 export const useAppTabs = () => {
-  const { appTabsUsed } = storeToRefs(store());
+  const { appTabsUsed, appBarHeightPX } = storeToRefs(store());
   appTabsUsed.value = true;
 
   onUnmounted(() => appTabsUsed.value = false);
 
-  return { ...commonSetup() };
+  return { appBarHeightPX };
 };
 
-export const renderAppTabs = () => ({ ...storeToRefs(store()), ...commonSetup() });
+export const renderAppTabs = () => storeToRefs(store());
