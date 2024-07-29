8aaae4756b3d8710fd47a5269f92e602b61ba565
Pinghao Wu <xdavidwuph@gmail.com>
2024-07-29 14:36:19 +0800

    DocsView: make external links open tabs

--- src/views/DocsView.vue
+++ src/views/DocsView.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import { VueComponent as Content } from '@/assets/docs.md';
 import StyledMarkdown from '@/components/StyledMarkdown.vue';
+
+const div = ref<HTMLDivElement>();
+
+onMounted(() => {
+  div.value?.querySelectorAll('a[href*="//"]').forEach((a) => {
+    a.setAttribute('target', '_blank');
+  });
+});
 </script>
 
 <template>
   <StyledMarkdown>
-    <Content />
+    <div ref="div">
+      <Content />
+    </div>
   </StyledMarkdown>
 </template>
