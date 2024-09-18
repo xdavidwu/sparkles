e302b20e37904d54fcf795f20e415ceea65781c5
Pinghao Wu <xdavidwuph@gmail.com>
2024-09-18 13:04:14 +0800

    progressReport: default progress to op title

--- src/utils/progressReport.ts
+++ src/utils/progressReport.ts
@@ -7,6 +7,7 @@ export const withProgress = async <T>(
   const { active, title, text } = storeToRefs(useProgress());
   active.value = true;
   title.value = t;
+  text.value = title.value;
   try {
     return await op((p) => text.value = p);
   } finally {
