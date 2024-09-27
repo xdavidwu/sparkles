728b026d5f0cb7d7e7f3559a767a9ca2a04fc167
Pinghao Wu <xdavidwuph@gmail.com>
2024-09-27 20:48:06 +0800

    humanDuration: refactor with table

--- src/utils/duration.ts
+++ src/utils/duration.ts
@@ -1,44 +1,33 @@
 // k8s.io/apimachinery/pkg/util/duration.HumanDuration
+const units = [
+  { name: 'm', of: 60, threshold: 2, detailsThreshold: 10 },
+  { name: 'h', of: 60, threshold: 3, detailsThreshold: 8 },
+  { name: 'd', of: 24, threshold: 2, detailsThreshold: 8 },
+  { name: 'y', of: 365, threshold: 2, detailsThreshold: 8 },
+];
 export const humanDuration = (d: number): string => {
   const seconds = Math.floor(d / 1000);
   if (seconds < -1) {
     return '<invalid>';
   } else if (seconds < 0) {
     return '0s';
-  } else if (seconds < 60 * 2) {
-    return `${seconds}s`;
   }
 
-  const minutes = Math.floor(seconds / 60);
-  if (minutes < 10) {
-    const s = seconds - minutes * 60;
-    return (s == 0) ? `${minutes}m` : `${minutes}m${s}s`;
-  } else if (minutes < 60 * 3) {
-    return `${minutes}m`;
+  let currentUnit = 's';
+  let value = seconds;
+  for (const unit of units) {
+    if (value < unit.of * unit.threshold) {
+      return `${value}${currentUnit}`;
+    }
+    const valueInUnit = Math.floor(value / unit.of);
+    if (valueInUnit < unit.detailsThreshold) {
+      const r = value - unit.of * valueInUnit;
+      return (r == 0) ? `${valueInUnit}${unit.name}` : `${valueInUnit}${unit.name}${r}${currentUnit}`;
+    }
+    value = valueInUnit;
+    currentUnit = unit.name;
   }
-
-  const hours = Math.floor(minutes / 60);
-  if (hours < 8) {
-    const m = minutes - hours * 60;
-    return (m == 0) ? `${hours}h` : `${hours}h${m}m`;
-  } else if (hours < 48) {
-    return `${hours}h`;
-  }
-
-  const days = Math.floor(hours / 24);
-  if (hours < 24 * 8) {
-    const h = hours - days * 24;
-    return (h == 0) ? `${days}d` : `${days}d${h}h`;
-  } else if (hours < 24 * 365 * 2) {
-    return `${days}d`;
-  }
-
-  const years = Math.floor(days / 365);
-  if (hours < 24 * 365 * 8) {
-    const d = days - years * 24;
-    return (d == 0) ? `${years}y` : `${years}y${d}d`;
-  }
-  return `${years}y`;
+  return `${value}${currentUnit}`;
 };
 
 const suffixMap: { [key: string]: number } = {
