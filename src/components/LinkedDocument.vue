0429a61bd7c1b869ae50d1744fca74130fd2f653
Pinghao Wu <xdavidwuph@gmail.com>
2024-09-28 13:26:06 +0800

    LinkedDocument: cleanup

--- src/components/LinkedDocument.vue
+++ src/components/LinkedDocument.vue
@@ -2,60 +2,35 @@
 import { computed } from 'vue';
 import { linkRegex } from '@/utils/text';
 
-const props = withDefaults(defineProps<{
+const props = defineProps<{
   text: string;
-  tag?: string;
-  paragraph?: boolean;
-}>(), {
-  tag: 'pre',
-});
+}>();
 
 const tokens = computed(() => {
   const tokens = [];
 
-  const paraRegex = new RegExp(/\n\n/, 'gd');
-  const processText = (t: string) => {
-    if (!props.paragraph) {
-      tokens.push({
-        type: 'plain',
-        value: t,
-      });
-      return;
-    }
-    let currentIndex = 0;
-    for (const match of t.matchAll(paraRegex)) {
-      tokens.push({
-        type: 'plain',
-        value: t.substring(currentIndex, match.indices![0][0]),
-      });
-      tokens.push({
-        type: 'sep',
-        value: '',
-      });
-      currentIndex = match.indices![0][1];
-    }
-    tokens.push({
-      type: 'plain',
-      value: t.substring(currentIndex, t.length),
-    });
-  }
-
   let currentIndex = 0;
   for (const match of props.text.matchAll(linkRegex)) {
-    processText(props.text.substring(currentIndex, match.indices![0][0]));
+    tokens.push({
+      type: 'plain',
+      value: props.text.substring(currentIndex, match.indices![0][0]),
+    });
     tokens.push({
       type: 'link',
       value: props.text.substring(match.indices![0][0], match.indices![0][1]),
     });
     currentIndex = match.indices![0][1];
   }
-  processText(props.text.substring(currentIndex, props.text.length));
+  tokens.push({
+    type: 'plain',
+    value: props.text.substring(currentIndex, props.text.length),
+  });
   return tokens;
 });
 </script>
 
 <template>
-  <component :is="tag" class="text-pre-wrap">
+  <div class="text-pre-wrap">
     <template v-for="(token, i) in tokens">
       <template v-if="token.type == 'plain'">
         {{ token.value }}
@@ -64,9 +39,8 @@ const tokens = computed(() => {
         class="text-break-all" :href="token.value">
         {{ token.value }}
       </a>
-      <div v-if="token.type == 'sep'" :key="i" class="my-2" />
     </template>
-  </component>
+  </div>
 </template>
 
 <style scoped>
