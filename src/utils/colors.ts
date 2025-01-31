a3db46fb87da9c347c89be72bae22f1cb16b09dc
Pinghao Wu <xdavidwuph@gmail.com>
2025-01-31 16:06:44 +0800

    utils: colors: note for color classes usage

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -57,6 +57,10 @@ export const hashColor = async (str: string, baseColors: Array<BaseColor>,
   return { color: base, variant: variants[hash % variants.length] };
 };
 
+// prefer this over css code when applicable
+// bg-* classes, which vuetify useColor uses, contains fixed fg colors
+// useColor also selects fg color by contrast if using css code for bg
+// but handpicked, fixed fg colors via classes should be better
 export const colorToClass = (color: Color): string => 
   color.variant === ColorVariant.Base ? kebabCase(color.color) :
     `${kebabCase(color.color)}-${color.variant.replace(/\d/, '-$&')}`;
